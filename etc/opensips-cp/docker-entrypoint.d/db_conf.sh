2ea58c4857d1b008da84f7ff6a25f196ea866720
Stefan Darius <94108614+dariusstefan@users.noreply.github.com>
2024-07-17 12:49:26 +0300

    Update set_id of rtpproxy to default set_id.

--- etc/opensips-cp/docker-entrypoint.d/db_conf.sh
+++ etc/opensips-cp/docker-entrypoint.d/db_conf.sh
@@ -13,14 +13,14 @@ fi
 
 mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e "
 INSERT INTO rtpengine (socket, set_id)
-SELECT 'udp:${RTPENGINE_IP}:${RTPENGINE_PORT}', 1
+SELECT 'udp:${RTPENGINE_IP}:${RTPENGINE_PORT}', 0
 WHERE NOT EXISTS (
     SELECT 1 FROM rtpengine WHERE socket = 'udp:${RTPENGINE_IP}:${RTPENGINE_PORT}'
 );"
 
 mysql -h ${MYSQL_IP} -u ${MYSQL_USER} -p"${MYSQL_PASSWORD}" -D ${MYSQL_DATABASE} -e "
 INSERT INTO rtpproxy_sockets (rtpproxy_sock, set_id)
-SELECT 'udp:${RTPPROXY_IP}:${RTPPROXY_PORT}', 1
+SELECT 'udp:${RTPPROXY_IP}:${RTPPROXY_PORT}', 0
 WHERE NOT EXISTS (
     SELECT 1 FROM rtpproxy_sockets WHERE rtpproxy_sock = 'udp:${RTPPROXY_IP}:${RTPPROXY_PORT}'
 );"
